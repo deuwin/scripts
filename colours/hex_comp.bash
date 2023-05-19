3adc3a44f76800f87c208c49de33ef1c63fda005
deuwin <wcully@gmail.com>
2023-05-19 20:32:43 +0100

    Add hex_comp.bash
    For a more manual comparison of colours.

---
+++ colours/hex_comp.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# hex_comp.bash
+#
+# Display a hexadecimal colour representation next to the supplied colour in 265
+# space. Assuming your terminal is able to display 24 bit colour.
+#
+# Licensed under CC BY-SA 3.0 (https://creativecommons.org/licenses/by-sa/3.0/)
+# https://unix.stackexchange.com/questions/269077/tput-setaf-color-table-how-to-determine-color-codes/269085#269085
+#
+
+hexcomp() {
+    hex=${1#"#"}
+    r=$(printf '0x%0.2s' "$hex")
+    g=$(printf '0x%0.2s' ${hex#??})
+    b=$(printf '0x%0.2s' ${hex#????})
+
+    printf 'original \e[48;2;%d;%d;%dm      \e[0m %s\n' "$r" "$g" "$b" "$1"
+    printf ' compare \e[48;5;%sm      \e[0m %s\n' "$2" "$2"
+}
+
+hexcomp "$1" "$2"
